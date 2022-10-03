6a6970f9c45832b98154e873fefaae8c9ea99ca0
DmitriH08 <113308027+DmitriH08@users.noreply.github.com>
2022-10-03 12:44:11 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slider').slick();
+});
+
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+  });
+
+
